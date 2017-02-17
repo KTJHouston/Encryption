bc46a1e591dd69bf3a879b82d048bf3fa1b22fe3
KTJHouston <ktjhouston@gmail.com>
2017-02-17 17:21:40 -0600

    Created getD(...)
    Deleted gcd(...).
    Improved getE(...).

--- src/Enc.java
+++ src/Enc.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Enc {
 
-	long p, q, N, phi, e, d;
+	long p, q, n, phi, e, d;
 	
 	/**
 	 * Constructor for Encryption
@@ -12,11 +12,12 @@ public class Enc {
 	public Enc( long p, long q ) {
 		this.p = p;
 		this.q = q;
-		N = p * q;
+		n = p * q;
 		phi = ( p - 1 ) * ( q - 1 );
-		e = gete( p - 1, q - 1 );
-		System.out.println(e);
-		//gcd( 3, 4 );
+		e = getE( p - 1, q - 1 );
+		System.out.println( "e: " + e );
+		System.out.println( "d: " + getD( e, phi ) );
+		System.out.println( "phi: " + phi );
 	}
 	
 	/**
@@ -25,7 +26,7 @@ public class Enc {
 	 * @param qmin q - 1
 	 * @return e such that gcd( phi, e ) = 1
 	 */
-	public long gete( long pmin, long qmin ) {
+	public long getE( long pmin, long qmin ) {
 		ArrayList<Long> pFactors = new ArrayList<Long>(), qFactors = new ArrayList<Long>();
 		while( pmin % 2 == 0 ) {
 			pmin /= 2;
@@ -61,23 +62,22 @@ public class Enc {
 		if( qmin > 1 ) {
 			qFactors.add((long)qmin);
 		}
-		//put both facts into one array list:
+		//put both factors into one array list:
 		ArrayList<Long> factors = pFactors;
 		for( long q : qFactors ) {
 			factors.add((long)q);
 		}
 		//find e such that gcd( phi, e ) = 1:
-		long e = 1, max = (long)Math.sqrt(phi);
+		long e = 1;
 		boolean timesTwo = true;
 		for( long f : factors) {
 			if( f == 2 ) {
 				timesTwo = false;
+				break;
 			}
 		}
-		if(timesTwo) {
-			e *= 2;
-		}
-		for( long i = 3; e < max; i+=2 ) {
+		e = (timesTwo) ? (e*2) : (e);
+		for( long i = 3; e < phi; i+=2 ) {
 			boolean mult = true;
 			for( long f : factors) {
 				if( i % f == 0 ) {
@@ -85,33 +85,32 @@ public class Enc {
 				}
 			}
 			if(mult) {
+				if( e * i > phi ) {
+					break;
+				}
 				e *= i;
 			}
 		}
+		
 		return e;
 	}
 	
 	/**
-	 * Finds the greatest common denominator of two integers. 
-	 * @param a integer
-	 * @param b integer
-	 * @return gcd( a, b)
+	 * Finds a d such that d*e is congruent to 1 (mod phi)
+	 * @param e
+	 * @param phi
+	 * @return
 	 */
-	public int gcd( int first, int second ) {
-		int a = first, b = second;
-		if( b > a ) {
-			int c = a;
-			a = b;
-			b = c;
-			System.out.println( a + ", " + b );
+	public long getD( long e, long phi ) {
+		long eMP = e,//e mod phi
+			 d = 1;
+		
+		while( eMP != 1 ) {
+			d++;
+			eMP = (eMP + e) % phi;
 		}
-		ArrayList<Integer> as = new ArrayList<Integer>(), bs = new ArrayList<Integer>(), qs = new ArrayList<Integer>(), rs = new ArrayList<Integer>();
-		as.add(a);
-		bs.add(b);
-		while( rs.get(rs.size()-1) != 1 ) {
-			
-		}
-		return first;
+		
+		return d;
 	}
 	
 }
